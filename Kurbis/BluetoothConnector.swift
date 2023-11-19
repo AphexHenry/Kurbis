a9ba08dadf4e89357955e63843a5525e94bc6fd3
baptiste bohelay <baptiste.bohelay@gmail.com>
2023-11-19 17:20:32 +0100

    first commit

---
+++ Kurbis/BluetoothConnector.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothConnector.swift
+//  Kurbis
+//
+//  Created by Baptiste Bohelay on 19/11/2023.
+//
+
+import Foundation
+
+class BluetoothConnector {
+    var property: String
+
+    // Initialization method
+    init(property: String) {
+        self.property = property
+    }
+}
---
+++ Kurbis/SimplebleWrapper.h
@@ -0,0 +1,29 @@
+//
+//  SimplebleWrapper.hpp
+//  Kurbis
+//
+//  Created by Baptiste Bohelay on 19/11/2023.
+//
+
+#ifndef SimplebleWrapper_hpp
+#define SimplebleWrapper_hpp
+
+class SimplebleCpp {
+public:
+    SimplebleCpp() {
+        init();
+    }
+    
+    void init();
+};
+
+#endif /* SimplebleWrapper_hpp */
+
+#import <Foundation/Foundation.h>
+
+@interface MyCppClassWrapper : NSObject
+
+@end
+
+
+
